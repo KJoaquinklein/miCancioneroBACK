2c9bc617e95e90c8338c2384e1f4709fd9a79cd3
KJoaquinklein <joa_klein1@hotmail.com>
2024-05-31 16:23:02 -0300

    tercer commit

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -1,10 +1,15 @@
 const { Sequelize } = require("sequelize");
 const UserModel = require("./models/User");
+const { DB_CONNECTION } = process.env;
 
-const sequelize = new Sequelize("postgres://postgres:Samictwd15@localhost/micancionero", {
+const sequelize = new Sequelize(DB_CONNECTION, {
     logging: false,
     native: false,
 });
+// const sequelize = new Sequelize("postgres://postgres:Samictwd15@localhost/micancionero", {
+//     logging: false,
+//     native: false,
+// });
 
 UserModel(sequelize);
 
