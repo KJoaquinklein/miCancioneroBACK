17348907713f15ad4261fda8f2cfea1c3c046657
KJoaquinklein <joa_klein1@hotmail.com>
2024-05-31 20:19:47 -0300

    cuarto commit

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -2,10 +2,13 @@ const { Sequelize } = require("sequelize");
 const UserModel = require("./models/User");
 const { DB_CONNECTION } = process.env;
 
-const sequelize = new Sequelize(DB_CONNECTION, {
-    logging: false,
-    native: false,
-});
+const sequelize = new Sequelize(
+    "PGPASSWORD=8kiECzlp2nfefVLRQCpds0c2CkmlF6Go psql -h dpg-cpd28rgl6cac73bdv17g-a.oregon-postgres.render.com -U micancionero_user micancionero",
+    {
+        logging: false,
+        native: false,
+    }
+);
 // const sequelize = new Sequelize("postgres://postgres:Samictwd15@localhost/micancionero", {
 //     logging: false,
 //     native: false,
