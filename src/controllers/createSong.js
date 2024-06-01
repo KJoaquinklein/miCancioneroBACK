567b10104e5722907266987c183315983b801ca9
KJoaquinklein <joa_klein1@hotmail.com>
2024-05-31 21:12:31 -0300

    noveno commit

--- src/controllers/createSong.js
+++ src/controllers/createSong.js
@@ -2,15 +2,22 @@ const { User } = require("../DB_connection");
 
 const createSong = async (req, res) => {
     try {
-        const { username, title, author, type, sections } = req.body;
+        const { username, title, author, genre, sections } = req.body;
 
-        if (!title || !author || !type || !sections) {
+        if (!title || !author || !genre || !sections) {
             return res.status(400).json({ message: "Faltan datos" });
         }
 
         const response = await User.findOne({ where: { username: username } });
 
-        await response.update({ songs: [...response.songs, { title, author, type, sections }] });
+        await response.update({ songs: [...response.songs, { title, author, genre, sections }] });
+
+        if (response.songs) {
+            await response.update({ songs: [...response.songs, { title, author, genre, sections }] });
+        } else {
+            await response.update({ songs: [{ title, author, genre, sections }] });
+        }
+
         return res.status(200).json({ message: "Canción creada exitosamente" });
     } catch (err) {
         return res.status(500).json({ error: err.message });
