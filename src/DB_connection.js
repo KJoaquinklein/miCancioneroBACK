97fac1b79a14b25b717dace551c11c95f4cd1042
KJoaquinklein <joa_klein1@hotmail.com>
2024-06-11 18:09:19 -0300

    modificacion conexion bdd

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -7,7 +7,7 @@ const sequelize = new Sequelize(
     "micancionero_user", // Usuario
     "8kiECzlp2nfefVLRQCpds0c2CkmlF6Go", // Contraseña
     {
-        host: "dpg-cpd28rgl6cac73bdv17g-a.oregon-postgres.render.com",
+        host: "postgres://micancionero_user:8kiECzlp2nfefVLRQCpds0c2CkmlF6Go@dpg-cpd28rgl6cac73bdv17g-a/micancionero",
         dialect: "postgres",
         dialectOptions: {
             ssl: {
