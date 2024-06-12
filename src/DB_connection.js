718e153916c080daf75d0878ead45604c8931c32
KJoaquinklein <joa_klein1@hotmail.com>
2024-06-12 13:04:16 -0300

    bdd corr 1

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -2,23 +2,23 @@ const { Sequelize } = require("sequelize");
 const SongModel = require("./models/Song");
 const { DB_CONNECTION } = process.env;
 
-// const sequelize = new Sequelize(
-//     "micancionero", // Nombre de la base de datos
-//     "micancionero_user", // Usuario
-//     "8kiECzlp2nfefVLRQCpds0c2CkmlF6Go", // Contraseña
-//     {
-//         host: "postgres://micancionero_user:8kiECzlp2nfefVLRQCpds0c2CkmlF6Go@dpg-cpd28rgl6cac73bdv17g-a/micancionero",
-//         dialect: "postgres",
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false,
-//             },
-//         },
-//         logging: false,
-//         native: false,
-//     }
-// );
+const sequelize = new Sequelize(
+    "micancionero", // Nombre de la base de datos
+    "micancionero_user", // Usuario
+    "8kiECzlp2nfefVLRQCpds0c2CkmlF6Go", // Contraseña
+    {
+        host: "postgres://micancionero_user:8kiECzlp2nfefVLRQCpds0c2CkmlF6Go@dpg-cpd28rgl6cac73bdv17g-a/micancionero",
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+        logging: false,
+        native: false,
+    }
+);
 
 // const sequelize = new Sequelize(
 //     "PGPASSWORD=8kiECzlp2nfefVLRQCpds0c2CkmlF6Go psql -h dpg-cpd28rgl6cac73bdv17g-a.oregon-postgres.render.com -U micancionero_user micancionero",
@@ -27,13 +27,13 @@ const { DB_CONNECTION } = process.env;
 //         native: false,
 //     }
 // );
-const sequelize = new Sequelize(
-    "postgres://micancionero_user:8kiECzlp2nfefVLRQCpds0c2CkmlF6Go@dpg-cpd28rgl6cac73bdv17g-a/micancionero",
-    {
-        logging: false,
-        native: false,
-    }
-);
+// const sequelize = new Sequelize(
+//     "postgres://micancionero_user:8kiECzlp2nfefVLRQCpds0c2CkmlF6Go@dpg-cpd28rgl6cac73bdv17g-a/micancionero",
+//     {
+//         logging: false,
+//         native: false,
+//     }
+// );
 
 SongModel(sequelize);
 
